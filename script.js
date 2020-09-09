3168f4b40f196a78e4170cbe0512790cbc590c22
RicoIro <112274@emmauscollege.nl>
2020-09-09 09:26:00 +0000

    3 ballen

--- script.js
+++ script.js
@@ -17,10 +17,17 @@ function setup() {
  * de code in deze functie wordt meerdere keren per seconde
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
+var x = 0;
+var y = 0;
 function draw() {
+    clear();
+    x = x + 0.1;
+    y = y + 0.1;
   // stel vulkleur in
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(300+250*sin(x),300+250*cos(y),80,80);
+  ellipse(300+250*sin(x),300+125*cos(y)*cos(y),80,80);
+  ellipse(300+125*sin(x)*sin(x),300+250*cos(y),80,80);
 }
